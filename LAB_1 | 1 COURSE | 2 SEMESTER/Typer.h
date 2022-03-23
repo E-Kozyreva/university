7cad06ba378358d8bdda74eb37516f5681a6f238
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-03-23 21:05:35 +0300

    code

---
+++ LAB_1 | 1 COURSE | 2 SEMESTER/Typer.h
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+
+class Transport {
+public:
+	int answer;
+	int SelectType() {
+		std::cout << "Select type of transport:\n";
+		std::cout << "1.Land.\n2.Water.\n3.Air.\n";
+		std::cout << "Your answer: ";
+		std::cin >> answer;
+		std::cout << "\n";
+
+		return answer;
+	}
+};
+
+class Land:public Transport {
+public:
+	std::string name;
+	std::string description;
+	int Description() {
+		name = "\nLand ";
+		description = "- transportation or movement of people, animals "
+			          "or goods from one place to another on land. The "
+                      "two main types of land transport can be considered "
+                      "railway and automobile.\n\n";
+		std::cout << "Do you want to read description?\n1.Yes.\n2.No.\n";
+		std::cout << "0.Exit.\n";
+		std::cout << "Your answer: ";
+		std::cin >> answer;
+		std::cout << "\n";
+
+		return answer;
+	}
+	void TypesOfTranspot() {
+		std::cout << "\nSelect a transport. Enter number of transport:\n";
+		std::string types[9] = {"Bus", "Taxi", "Train",
+								 "Car", "Motorcycle", "Subway",
+								 "Snowmobile", "Trolleybus", "Tram",};
+		for (int num = 1; num < 10; num++) {
+			std::cout << num << "." << types[num - 1] << ".\n";
+		}
+	}
+	int SelectTypeOfTransport() {
+        std::cout << "0.Exit.\n";
+		std::cout << "Your answer: ";
+		std::cin >> answer;
+		std::cout << "\n";
+
+		return answer;
+	}
+};
+
+class Water:public Transport {
+public:
+	std::string name;
+	std::string description;
+	int Description() {
+		name = "\nWater ";
+		description = "- a type of transport that transports cargo "
+			          "and/or passengers along waterways, both natural "
+			          "(oceans, seas, rivers, lakes) and artificial "
+			          "(canals, reservoirs).\n\n";
+		std::cout << "Do you want to read description?\n1.Yes.\n2.No.\n";
+		std::cout << "0.Exit.\n";
+		std::cout << "Your answer: ";
+		std::cin >> answer;
+		std::cout << "\n";
+
+		return answer;
+	}
+	void TypesOfTranspot() {
+		std::cout << "\nSelect a transport. Enter number of transport:\n";
+		std::string types[6] = {"Ship", "Ferry", "Boat", 
+			                    "Motor ship", "Yacht", "Vessel", };
+		for (int num = 1; num < 7; num++) {
+			std::cout << num << "." << types[num - 1] << ".\n";
+		}
+	}
+	int SelectTypeOfTransport() {
+		std::cout << "0.Exit.\n";
+		std::cout << "Your answer: ";
+		std::cin >> answer;
+		std::cout << "\n";
+
+		return answer;
+	}
+};
+
+class Air:public Transport {
+public:
+	std::string name;
+	std::string description;
+	int Description() {
+		name = "\nAir ";
+		description = "- a type of transport that transports cargo "
+			          "and passengers by air with the help of aircraft: "
+			          "airplanes, helicopters, etc.; helps internationalization "
+			          "and globalization of all aspects of human activity.\n\n";
+		std::cout << "Do you want to read description?\n1.Yes.\n2.No.\n";
+		std::cout << "0.Exit.\n";
+		std::cout << "Your answer: ";
+		std::cin >> answer;
+		std::cout << "\n";
+
+		return answer;
+	}
+	void TypesOfTranspot() {
+		std::cout << "\nSelect a transport. Enter number of transport:\n";
+		std::string types[3] = {"Plane", "Helicopter", "Balloon"};
+		for (int num = 1; num < 4; num++) {
+			std::cout << num << "." << types[num - 1] << ".\n";
+		}
+	}
+	int SelectTypeOfTransport() {
+		std::cout << "0.Exit.\n";
+		std::cout << "Your answer: ";
+		std::cin >> answer;
+		std::cout << "\n";
+
+		return answer;
+	}
+};
