9f40df355fa52f8e663f1ded06c1b068992367bb
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-04-10 16:28:10 +0300

    code - class

---
+++ LAB_2 | 1 COURSE | 2 SEMESTER/data complex.h
@@ -0,0 +1,27 @@
+class TComplex {
+private:
+	double ActualPart1, ImaginaryPart1;
+	double ActualPart2, ImaginaryPart2;
+public:
+	TComplex (double a1, double e1, double a2, double e2) {
+		ActualPart1 = a1;
+		ImaginaryPart1 = e1;
+		ActualPart2 = a2;
+		ImaginaryPart2 = e2;
+	}
+	void Output(double ActualPart, double ImaginaryPart);
+
+	void AdditionComplex();
+	void SubtractionComplex();
+	void MultiplicationComplex();
+	void DivisionComplex();
+	void AssignmentComplex();
+	void ComparisonComplex();
+
+	void ModuleComplex();
+	void PowerPComplex();
+	void PowerMComplex();
+	void PowerDComplex();
+	void PowerCComplex();
+	void TrigonometricComplex();
+};
