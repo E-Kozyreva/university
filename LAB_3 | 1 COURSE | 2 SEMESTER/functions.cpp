951c40ce6f494d1717a78e6805531893ecac8d62
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-04-17 13:26:28 +0300

    code | functions

--- LAB_3 | 1 COURSE | 2 SEMESTER/functions.cpp
+++ LAB_3 | 1 COURSE | 2 SEMESTER/functions.cpp
@@ -2,6 +2,13 @@
 #include "functions.h"
 #include "classes.h"
 
+int Functions();
+std::string GetString();
+int LineOptions();
+void GetOption(int answer, std::string str1, std::string str2);
+void Restart(int answer, std::string str1, std::string str2);
+
+
 int Functions() {
 	int answer;
 	std::cout << "Select one of the functions:\n";
@@ -29,17 +36,21 @@ std::string GetString() {
 int LineOptions() {
 	int answer;
 	std::cout << "Select one of the options:\n";
-	std::cout << "1..\n";
-	std::cout << "1..\n";
-	std::cout << "1..\n";
-	std::cout << "1..\n";
+	std::cout << "1.Add strings.\n";
+	std::cout << "2.Copy strings.\n";
+	std::cout << "3.Compare strings.\n";
+	std::cout << "4.Which string is bigger.\n";
+	std::cout << "5.Find the index of the entered string.\n";
+	std::cout << "6.Copy a string multiple times.\n";
+	std::cout << "7.Find an item in string.\n";
+	std::cout << "Your answer: ";
 	std::cin >> answer;
 	std::cout << "\n";
 
-	if (answer > 0 && answer < 6) {
+	if (answer > 0 && answer < 8) {
 		return answer;
 	} else {
-		std::cout << "Wong answer, try again!\n\n";
+		std::cout << "Wrong answer, try again!\n\n";
 		LineOptions();
 	}
 	return 0;
@@ -48,16 +59,37 @@ void GetOption(int answer, std::string str1, std::string str2) {
 	TStrings strings{str1, str2};
 
 	if (answer == 1) {
-		strings.AddLines();
+		strings.AddStrings();
 	} else if (answer == 2) {
-		strings.EquateLines();
+		strings.EquateStrings();
 	} else if (answer == 3) {
-		strings.CompareLines();
+		strings.CompareStrings();
 	} else if (answer == 4) {
-		strings.BoSLines();
-	} else if (answer == 5) {
-		strings.DublicateLine();
+		strings.BMString();
+	} else if (answer == 5){
+		strings.IndexString();
 	} else if (answer == 6) {
-		strings.FindLine();
+		strings.DublicateString();
+	} else if (answer == 7) {
+		strings.FindString();
+	}
+	std::cout << "\n";
+	Restart(answer, str1,str2);
+}
+void Restart(int answer, std::string str1, std::string str2) {
+	std::cout << "Do you want to do something else?\n";
+	std::cout << "1.Yes.\n2.No.\n";
+	std::cout << "Your answer: ";
+	std::cin >> answer;
+	std::cout << "\n";
+
+	if (answer == 1) {
+		int option = LineOptions();
+		GetOption(option, str1, str2);
+	} else if (answer == 2) {
+		std::cout << "Okay, goodbye!\n\n";
+	} else {
+		std::cout << "Wrong answer, try again!\n\n";
+		Restart(answer, str1, str2);
 	}
 }
