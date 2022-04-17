8c563ba8f5efd19f1f552cb9befeb3a3cde23155
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-04-17 13:24:07 +0300

    code

--- LAB_3 | 1 COURSE | 2 SEMESTER/class.h
+++ LAB_3 | 1 COURSE | 2 SEMESTER/class.h
@@ -5,22 +5,21 @@ private:
 	std::string string2;
 public:
 	int answer;
+	std::string string;
 
 	TStrings(std::string& str1, std::string& str2) {
 		string1 = str1;
 		string2 = str2;
 	}
 
-	int Options();
-	void Restart();
+	void AddStrings();
+	void EquateStrings();
+	void CompareStrings();
+	void BMString();
+	void IndexString();
 
-	void AddLines();
-	void EquateLines();
-	void CompareLines();
-	void BoSLines();
-
-	void DublicateLine();
-	void FindLine();
+	void DublicateString();
+	void FindString();
 
 	~TStrings();
 };
